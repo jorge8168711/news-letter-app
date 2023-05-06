e740f3ebf0c8271c973c98525c007a773e0b90db
Jorge Barron <jorgeb8168711@gmail.com>
2023-05-06 00:07:37 -0600

    Client server connection

--- server/server.js
+++ server/server.js
@@ -1,10 +1,17 @@
 const express = require('express')
 
 const app = express()
-const port = 3000
+const port = 5000
 
 app.get('/api', (req, res) => {
-  res.send({ users: [{ name: 'John Doe', id: 1 }, { name: 'Juan Doe', id: 2 }] })
+  res.send(
+    JSON.stringify({
+      users: [
+        { name: 'John Doe', id: 1 },
+        { name: 'Juan Doe', id: 2 }
+      ]
+    })
+  )
 })
 
 app.listen(port, () => {
