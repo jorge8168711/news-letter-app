5f1e1593738163832dc251ae4c5da68d001961b6
Jorge Barron <jorgeb8168711@gmail.com>
2023-05-05 23:54:16 -0600

    Initial client app

--- server/server.js
+++ server/server.js
@@ -3,8 +3,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/api', (req, res) => {
+  res.send({ users: [{ name: 'John Doe', id: 1 }, { name: 'Juan Doe', id: 2 }] })
 })
 
 app.listen(port, () => {
